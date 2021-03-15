cc95fcd65543779449d517218e4c6d9a5f6e75e6
Luxa-pixel-hub <80690178+Luxa-pixel-hub@users.noreply.github.com>
2021-03-15 13:45:46 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,14 +4,14 @@ module.exports = {
   code: `
 $ban[$mentioned[1]]
 $color[#FF0000]
-$footer[Success Banned User]
-$author[Its a bird, Its a plane, Its a ban hammer!]
-$description[Ban hammer succesfully used!
+$footer[BENUTZER WURDE ERFOLGREICH GEBANNT!]
+$author[Das hat er jetzt davon!]
+$description[Du hast soeben einen Benutzer erfolgreich vom Server gebannt!
 $addField[Banned;<@$mentioned[1]>]
 $addField[Admin;<@$authorID>]
 $addField[Reason;$noMentionMessage]
 ]
 $argsCheck[>2;Use: \`$getServerVar[prefix]ban <@user> <Reason>\`]
-$onlyPerms[manageserver;❌ **You need Manage Server Permissions!**]
+$onlyPerms[manageserver;❌ **Du darfst diesen Befehl nicht ausführen!**]
 `
 }
